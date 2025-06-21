a769b9bf7db48ffec74acdd47b602471c3dcd1fb
afro-juliano <afro.juliano@gmail.com>
2025-06-21 00:37:28 -0300

    adicionado o provider hashicorp/local

--- IaC/version.tf
+++ IaC/version.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.5.3"
+    }
     cloudstack = {
       source = "cloudstack/cloudstack"
       version = "0.5.0"
