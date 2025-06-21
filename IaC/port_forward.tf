c2b9bc24acecfa81e6d4d29b24e0041b0794731d
afro-juliano <afro.juliano@gmail.com>
2025-06-21 11:18:27 -0300

    alterado port_forward

--- IaC/port_forward.tf
+++ IaC/port_forward.tf
@@ -1,21 +1,21 @@
 resource "cloudstack_port_forward" "port_forward" {
   ip_address_id = cloudstack_ipaddress.public_ip.id
 
-  rule = {
+  forward {
     protocol = "tcp"
     private_port = "22"
     public_port = "2222"
     virtual_machine_id = cloudstack_instance.Swarm-01.id
   }
 
-  rule = {
+  forward {
     protocol = "tcp"
     private_port = "22"
     public_port = "2223"
     virtual_machine_id = cloudstack_instance.Swarm-02.id
   }
 
-  rule = {
+  forward {
     protocol = "tcp"
     private_port = "22"
     public_port = "2224"
