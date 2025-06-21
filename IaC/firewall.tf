800bcd16e378808f8ac8ef939137d106d0c3ecc8
afro-juliano <afro.juliano@gmail.com>
2025-06-20 23:55:24 -0300

    adicionada regra para o meu ip para o SSH

--- IaC/firewall.tf
+++ IaC/firewall.tf
@@ -3,7 +3,13 @@ resource "cloudstack_firewall" "fw" {
 
   rule {
     cidr_list = ["0.0.0.0/0"]
-    protocol = "tcp"
-    ports = ["2222-2224"]
+    protocol  = "tcp"
+    ports     = ["80", "443"]
+  }
+  
+  rule {
+    cidr_list = ["186.249.196.160/32"]
+    protocol  = "tcp"
+    ports     = ["2222-2224"]
   }
 }
